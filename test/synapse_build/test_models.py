812c1af766116010a8fa7052c158c1374653ad2a
Bjarne Meyn <UI930057@rwe-ag.com>
2022-06-06 08:09:44 +0000

    implemented tests for synapse build models and build

---
+++ test/synapse_build/test_models.py
@@ -0,0 +1,48 @@
+import pytest
+from src.synapse_build.models import *
+from pytest_cases import parametrize_with_cases
+from .test_models_cases import TestCases_DeploymentArtifacts, TestCases_DeploymentParameters,TestCases_ParameterConfig
+
+
+@parametrize_with_cases("success,inputs,expected", cases=TestCases_DeploymentArtifacts)
+def test_DeploymentArtifacts(success,inputs,expected):
+    
+    if success:
+        # test for successful run
+        deployment_artifacts = DeploymentArtifacts(
+            content_version=inputs["content_version"],
+            schema=inputs["schema"],
+            source_workspace=inputs["source_workspace"] )
+        assert deployment_artifacts.to_json() == expected
+        
+    elif not success:        
+        
+        # test error handling
+        with pytest.raises(expected["error"], match=expected["message"]):
+            deployment_artifacts = DeploymentArtifacts(
+                content_version=inputs["content_version"],
+                schema=inputs["schema"],
+                source_workspace=inputs["source_workspace"] )
+
+@parametrize_with_cases("inputs,expected", cases=TestCases_DeploymentParameters)
+def test_DeploymentParameters(inputs, expected):
+
+    deployment_parameters = DeploymentParameters(
+        content_version=inputs["content_version"] ,
+        schema=inputs["schema"],
+        source_workspace=inputs["source_workspace"] )
+
+    assert deployment_parameters.to_json() == expected
+
+
+@parametrize_with_cases("inputs,expected", cases=TestCases_ParameterConfig)
+def test_ParameterConfig(inputs, expected):
+
+    # initialize ParameterConfig
+    config = ParameterConfig(config_path=inputs)
+    
+    # load expected config file from fixtures
+    # with open(os.path.join("test/synapse_build/fixtures/", exp_config_path), 'r', encoding="utf-8") as f:
+    #     expected_config_file = json.load(f)
+
+    assert config.to_json() == expected
---
+++ test/synapse_build/test_models_cases.py
@@ -0,0 +1,230 @@
+import json
+import os
+
+class TestCases_DeploymentArtifacts():
+
+    def case_1_DeploymentArtifacts_pass_default_config(self):
+        success = True
+        inputs = {
+            "content_version": "1.0.0.0",
+            "schema": "2015-01-01",
+            "source_workspace": "my-syn-synaps-dev"
+        }
+        expected = {  
+            "$schema": 'http://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#',
+            "contentVersion": "1.0.0.0",
+            "resources": [],
+            "parameters":  {      
+                "workspaceName": {
+                    "type": "string",
+                    "metadata": "Workspace name",
+                    "defaultValue": "my-syn-synaps-dev"}
+            },
+            "variables": {
+                "workspaceId": "[concat('Microsoft.Synapse/workspaces/', parameters('workspaceName'))]"}
+        }
+        return success, inputs, expected
+
+    def case_2_DeploymentArtifacts_fail_no_source_workspace(self):
+        success = False
+        inputs = {
+            "content_version":  "1.0.0.0",
+            "schema": "2015-01-01",
+            "source_workspace": None
+        }
+        expected = {
+            "error": TypeError,
+            "message": "source_workspace attribute must be set to an instance of <class 'str'>" 
+        }
+
+        return success, inputs, expected
+
+    def case_3_DeploymentArtifacts_fail_no_schema(self):
+        success = False
+        inputs = {
+            "content_version":  "1.0.0.0",
+            "schema": None,
+            "source_workspace": "test-name"
+        }
+        expected = {
+            "error": TypeError,
+            "message": "schema attribute must be set to an instance of <class 'str'>" 
+        }
+
+        return success, inputs, expected
+
+    def case_4_DeploymentArtifacts_fail_no_content_version(self):
+        success = False
+        inputs = {
+            "content_version":  None,
+            "schema": "2015-01-01",
+            "source_workspace": "test-name"
+        }
+        expected = {
+            "error": TypeError,
+            "message": "content_version attribute must be set to an instance of <class 'str'>" 
+        }
+
+        return success, inputs, expected
+
+
+class TestCases_DeploymentParameters():
+
+    def case_1_default_config(self):
+        inputs =  {
+              "content_version": "1.0.0.0",
+              "schema": "2015-01-01",
+              "source_workspace": "my-syn-synaps-dev"
+        }
+        expected = {"$schema": 'https://schema.management.azure.com/schemas/2015-01-01/deploymentParameters.json#',"contentVersion": "1.0.0.0","parameters":  {"workspaceName": {"value": "my-syn-synaps-dev"}}}
+        return inputs, expected
+
+class TestCases_ParameterConfig():
+
+    def save_config(self, config, directory, file_name):
+        """Save test config to file to be used in the test case
+
+        Args:
+            config (dict): config object
+
+        Returns:
+            str: path to the saved config file
+        """
+        
+        config_path = os.path.join(directory, file_name)
+        
+        # create tmp directory when not exists
+        if not os.path.exists("tmp"):
+            os.makedirs("tmp")
+
+        with open(config_path, 'w', encoding="utf-8") as outfile:
+            json.dump(config, outfile)
+
+        return config_path
+
+    def case_1_default_config(self):
+
+        # define input config
+        # save input config to file
+        input_config_path = ""
+        # define expected config
+        expected = {
+            "Microsoft.Synapse/workspaces/notebooks": {
+                "properties": {
+                    "bigDataPool": {
+                        "referenceName": "="}}},
+            "Microsoft.Synapse/workspaces/sqlscripts": {
+                "properties": {
+                    "content": {
+                        "currentConnection": {
+                            "*": "-"}}}},
+            "Microsoft.Synapse/workspaces/pipelines": {
+                "properties": {
+                    "activities": [
+                        {
+                            "typeProperties": {
+                                "waitTimeInSeconds": "-::int",
+                                "headers": "=::object"}}]}},
+            "Microsoft.Synapse/workspaces/integrationRuntimes": {
+                "properties": {
+                    "typeProperties": {
+                        "*": "="}}},
+            "Microsoft.Synapse/workspaces/triggers": {
+                "properties": {
+                    "typeProperties": {
+                        "recurrence": {
+                            "*": "=",
+                            "interval": "=:triggerSuffix:int",
+                            "frequency": "=:-freq"
+                        },
+                        "maxConcurrency": "="}}},
+            "Microsoft.Synapse/workspaces/linkedServices": {
+                "*": {
+                    "properties": {
+                        "typeProperties": {
+                            "*": "="}}},
+                "AzureDataLakeStore": {
+                    "properties": {
+                        "typeProperties": {
+                            "dataLakeStoreUri": "="}}}},
+            "Microsoft.Synapse/workspaces/datasets": {
+                "properties": {
+                    "typeProperties": {
+                        "*": "="}}}
+        }
+
+        return input_config_path, expected
+    
+    def case_2_custom_config(self):
+
+        input_config = {
+            "Microsoft.Synapse/workspaces/notebooks": {
+                    "properties": {}
+                },
+                "Microsoft.Synapse/workspaces/sqlscripts": {
+                "properties": {
+                    "content":{
+                        "currentConnection":{
+                                "*":"="
+                            }
+                        } 
+                    }
+                },
+                "Microsoft.Synapse/workspaces/pipelines": {
+                    "properties": {
+                        "activities": [{
+                            "typeProperties": {
+                            }
+                        }],
+                    "parameters": {
+                        "storage_name": {
+                            "type": "=",
+                            "defaultValue": "="
+                        }
+                    }
+                    }
+                },
+                "Microsoft.Synapse/workspaces/integrationRuntimes": {
+                    "properties": {
+                        "typeProperties": {
+                            "*": "="
+                        }
+                    }
+                },
+                "Microsoft.Synapse/workspaces/triggers": {
+                    "properties": {
+                        "typeProperties": {
+                            "recurrence": {
+                                "*": "=",
+                                "interval": "=:triggerSuffix:int",
+                                "frequency": "=:-freq"
+                            },
+                            "maxConcurrency": "="
+                        }
+                    }
+                },
+                "Microsoft.Synapse/workspaces/linkedServices": {
+                    "*": {
+                        "properties": {
+                            "typeProperties": {
+                                "url": "=",
+                                "baseUrl": "=",
+                                "functionAppUrl": "="
+                            }
+                        }
+                    },
+                    "AzureDataLakeStore": {
+                        "properties": {
+                            "typeProperties": {
+                                "dataLakeStoreUri": "="
+                            }
+                        }
+                    }
+                },
+                "Microsoft.Synapse/workspaces/datasets": {
+                    "properties": {}
+                }
+            }
+        inputs = self.save_config(input_config,directory="tmp", file_name="case_2_custom_config.json")
+        expected = input_config
+        return inputs, expected
